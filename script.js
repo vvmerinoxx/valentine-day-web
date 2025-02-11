7e64b5350eb01a7d136981fb5ad43f157db92730
Victor Merino Ruiz <vvmerino@mercadona.es>
2025-02-11 20:39:31 +0100

    feat: pleased to please you

--- script.js
+++ script.js
@@ -2,13 +2,12 @@ document.addEventListener("DOMContentLoaded", function() {
     const noButton = document.getElementById("no");
 
     noButton.addEventListener("mouseover", function() {
-        const maxX = window.innerWidth - noButton.offsetWidth;
-        const maxY = window.innerHeight - noButton.offsetHeight;
+        const maxX = window.innerWidth - noButton.offsetWidth - 20; // Evita que salga de la pantalla
+        const maxY = window.innerHeight - noButton.offsetHeight - 20;
 
         let randomX = Math.random() * maxX;
         let randomY = Math.random() * maxY;
 
-        // Asegurar que el botón "No" no salga de la pantalla
         noButton.style.left = `${randomX}px`;
         noButton.style.top = `${randomY}px`;
     });
